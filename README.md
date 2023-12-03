474071021c88f329d78078bff629d614689b8830
Derek Boucher <derek.boucher@me.com>
2023-12-03 11:10:52 -0500

    feat: implemented solution of part 1 / day 1

---
+++ README.md
@@ -0,0 +1,12 @@
+# Advent of Code 2023 🌟🎄🦀
+
+## Day 1
+To run the solution:
+```rust
+RUST_LOG=info cargo run -p day1 -- -p ./day1/input.txt
+```
+
+Part 1 solution:
+```
+Calibration sum: 55002
+```
--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,90 @@
+extern crate log;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Path towards the input file
+    #[arg(short, long)]
+    path: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+    log::info!("Advent of code 2023!!!");
+    log::info!("-----------------------------");
+    log::info!("Day 1: Report Repair");
+
+    let args = Args::parse();
+    log::info!("Loading input file: {}", args.path);
+
+    let input = match std::fs::read_to_string(args.path) {
+        Ok(input) => input,
+        Err(e) => {
+            log::error!("Error while reading input file: {}", e);
+            return;
+        }
+    };
+
+    let seperate_lines: Vec<String> = input.lines().map(|line| String::from(line)).collect();
+
+    let mut calibration_sum = 0;
+
+    for line in seperate_lines {
+        let chars: Vec<char> = line.chars().map(|char| char).collect();
+
+        // Use a two pointer solution -> one at the begining of the string and one at the end to find the enclosing integers
+        let mut left = 0;
+        let mut right = chars.len() - 1;
+
+        while left <= right {
+            let left_char = chars[left];
+            let right_char = chars[right];
+
+            if left_char.is_numeric() && right_char.is_numeric() {
+                let left_int = left_char.to_digit(10).unwrap();
+                let right_int = right_char.to_digit(10).unwrap();
+
+                log::debug!("left: {}", left_int);
+                log::debug!("right: {}", right_int);
+
+                let mut calibration_value = left_int * 10;
+                calibration_value += right_int;
+                log::debug!("calibration value: {}", calibration_value);
+
+                calibration_sum += calibration_value;
+                break;
+            }
+
+            // Handle cases where there's only one integer
+            // in the string.
+            if left == right {
+                let mut unique = 0;
+                if left_char.is_numeric() {
+                    unique = left_char.to_digit(10).unwrap();
+                }
+
+                if right_char.is_numeric() {
+                    unique = right_char.to_digit(10).unwrap();
+                }
+
+                let mut calibration_value = unique * 10;
+                calibration_value += unique;
+
+                log::debug!("Unique integer value: {}", unique);
+                log::debug!("Unique integer calibration unit: {}", calibration_value);
+                calibration_sum += calibration_value;
+            }
+
+            if !left_char.is_numeric() {
+                left += 1;
+            }
+
+            if !right_char.is_numeric() {
+                right -= 1;
+            }
+        }
+    }
+
+    log::info!("Calibration sum: {}", calibration_sum);
 }
